d4014a4a159a9077e68a12f33b3a559b71afd6bd
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-08-18 22:45:29 -0300

    adicionado a biblioteca flask-jwt

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-#from flask_migrate import Migrate
-
+from flask_jwt_extended  import JWTManager
+import os
 """ import jwt
  """
-api = Flask(__name__)
 
+api = Flask(__name__)
+jwt = JWTManager(api)
+
+api.config['JWT_SECRET_KEY'] = os.environ.get('key_jwt','Key')
 api.config['JSON_AS_ASCII'] = False
 api.config['PORT'] = 5000
 api.config['DEBUG'] = True
