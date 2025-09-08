68534e8852e90f202bf17a0b62dcbbbd2d9369ac
Gabriel <gabriel.carvalhovieira123@gmail.com>
2025-09-08 18:37:55 -0300

    feat: blueprint de autenticação

--- run.py
+++ run.py
@@ -2,10 +2,11 @@ from app import db,api
 from endpoints.barber_routes import barber_blueprint
 from endpoints.cliente_routes import client_blueprint
 from endpoints.scheduling_routes import agendamento_blueprint
-
+from endpoints.auth_routes import auth_blueprint
 
 api.register_blueprint(barber_blueprint)
 api.register_blueprint(client_blueprint)
+api.register_blueprint(auth_blueprint)
 api.register_blueprint(agendamento_blueprint)
 
 with api.app_context():
